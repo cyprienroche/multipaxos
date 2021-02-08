f40e8506eb1ffac4c91b77e5641a1f31096778fd
Cyprien <roche.cyprien@gmail.com>
2021-02-08 11:27:22 +0100

    Implement Acceptor and allow it to receive requests but does not process them yet

--- lib/acceptor.ex
+++ lib/acceptor.ex
@@ -1,10 +1,36 @@
 
 # cyprienroche 12 feb 2021
 
+defmodule AcceptorState do
+  @enforce_keys [ :config ]
+  defstruct(
+    config: Map.new,
+    ballot_num: nil,
+    accepted: MapSet.new)
+
+	def new(config) do
+		%AcceptorState{
+      config: config }
+	end # new
+
+end # AcceptorState
+
 defmodule Acceptor do
 
 def start config do
-  Debug.info(config, "Starting Acceptor#{config.node_num}")
+  next AcceptorState.new(config)
 end # start
+
+defp next state do
+  receive do
+    { :P1A, _from, ballot_num } ->
+      ballot_num
+
+    { :P2A, _from, { ballot_num, _, _ } = pvalue } ->
+      pvalue
+  end # receive
+
+  next state
+end # next
 
 end # Acceptor
